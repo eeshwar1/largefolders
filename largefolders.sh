2e18bc6a1b74ed47fef5d4b18c937b9aa0804bff
Easwar <eeshwar1@gmail.com>
2021-12-18 14:07:45 -0600

    first commit

---
+++ largefolders.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+NUM_ITEMS=30
+NUM_LEVELS=3
+CURRENT_LEVEL=1
+
+find_large_folders() 
+{
+
+    local _folder=$1
+    local _current_level=$2
+
+    _opsizelist=$(du -skh $_folder/* | sort -hr | head -$NUM_ITEMS | awk '{printf "%s,",$1}')
+    _opfolderlist=$(du -skh $_folder/* | sort -hr | head -$NUM_ITEMS | awk '{printf "%s,",$2}')
+
+    OIFS="$IFS"
+    IFS=','
+    read -r -a _op_size_list <<< "$_opsizelist"
+    read -r -a _op_folder_list <<< "$_opfolderlist"
+    IFS="$OIFS"
+
+    ((_current_level=_current_level+1))
+    
+
+    _pre_str=''
+    for ((_l=0; _l<_current_level; ++_l));
+    do
+        _pre_str="$_pre_str---"
+    done
+    _pre_str="$_pre_str>"
+
+    for _i in ${!_op_folder_list[@]}
+    do
+        if [ -d "${_op_folder_list[$_i]}" ]; then
+            
+            echo "$_pre_str ${_op_size_list[$_i]}  >>> ${_op_folder_list[$_i]}"
+            if [ $NUM_LEVELS -gt $_current_level ]; then
+                if [ $(ls ${_op_folder_list[$_i]} | wc -l) -gt 0 ]; then
+                    find_large_folders "${_op_folder_list[$_i]}" "$_current_level"
+                fi
+            fi
+        fi
+    done
+}
+
+function is_empty {
+ [ "$1/"* "" = "" ]  2> /dev/null &&
+ [ "$1/"..?* "" = "" ]  2> /dev/null &&
+ [ "$1/".[^.]* "" = "" ]  2> /dev/null ||
+ [ "$1/"* = "$1/*" ]  2> /dev/null && [ ! -e "$1/*" ] &&
+ [ "$1/".[^.]* = "$1/.[^.]*" ]  2> /dev/null && [ ! -e "$1/.[^.]*" ] &&
+ [ "$1/"..?* = "$1/..?*" ]  2> /dev/null && [ ! -e "$1/..?*" ]
+}
+
+opsizelist=$(du -skh $1* | sort -hr | head -$NUM_ITEMS | awk '{printf "%s,",$1}')
+opfolderlist=$(du -skh $1* | sort -hr | head -$NUM_ITEMS | awk '{printf "%s,",$2}')
+
+OIFS="$IFS"
+IFS=','
+read -r -a op_size_list <<< "$opsizelist"
+read -r -a op_folder_list <<< "$opfolderlist"
+IFS="$OIFS"
+echo "count -> ${#op_folder_list[*]}"
+
+
+# for i in ${!op_folder_list[@]}
+# do
+#     echo "[$i] ${op_folder_list[$i]}"
+
+# done
+
+# exit 0
+
+# op_size_list=(${opsizelist//\n/})
+# op_folder_list=(${opfolderlist//\n/})
+
+for i in ${!op_folder_list[@]}
+do
+    # echo "!! ${op_folder_list[$i]} !!"
+    if [ -d "${op_folder_list[$i]}" ]; then
+        echo "---> ${op_size_list[$i]}  >>> ${op_folder_list[$i]}"
+        if [ $NUM_LEVELS -gt $CURRENT_LEVEL ]; then
+            if [ $(ls ${op_folder_list[$i]} | wc -l) -gt 0 ]; then
+                find_large_folders "${op_folder_list[$i]}" "$CURRENT_LEVEL"
+            fi
+        fi
+    fi
+done
+
---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+_level=3
+for i in $(seq 1 $_level);
+do
+    echo "Hello"
+done
